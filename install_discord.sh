2cc3fc8d10440725a1fad8bf580f6ff08a0ea5ee
dead-tech <dev.dead.tech@gmail.com>
2022-01-16 17:04:29 +0100

    :bug: Bug fix, move to snap for installing discord

--- install_discord.sh
+++ install_discord.sh
@@ -2,15 +2,10 @@
 
 set -xe
 
-sudo apt update && 
-sudo apt install -y gdebi-core wget &&
-wget -O ~/.dotfiles/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" &&
-sudo gdebi ~/.dotfiles/discord.deb
+sudo snap install discord
 
 if [ $? -ne 0 ]
 then
     echo "Unable to install discord..."
     exit 1
 fi
-
-rm ~/.dotfiles/discord.deb
